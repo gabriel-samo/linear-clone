430ed541d1e00b282514701de67f87f7ce28a63c
Gabriel Samoylov <gsamo122@gmail.com>
2024-09-26 18:55:05 +0300

    tailwind basic config, conatiner component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,18 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: '"SF Pro Display",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Helvetica Neue",sans-serif'
       },
-    },
+      colors: {
+        background: "#000212"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
