8aecb0328d65ef51ef6ac168ec0f3c78cef225f5
Gabriel Samoylov <gsamo122@gmail.com>
2024-10-01 22:33:20 +0300

    planet with stars section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { HeroImage } from "@/components/hero-image";
 import { ChevronIcon } from "@/components/icons/chevron";
 import { Button, IconWrapper } from "@/components/button";
 import { Hero, HeroSubtitle, HeroTitle } from "@/components/hero";
+import { StarsIllustration } from "@/components/icons/stars";
+import classNames from "classnames";
 
 export default function Home() {
   return (
@@ -46,6 +48,15 @@ export default function Home() {
       <Container>
         <Clients />
       </Container>
+      <div
+        className={classNames(
+          "mask-radial-faded relative my-[-12.8rem] h-[60rem] overflow-hidden",
+          "[--color:#7877C6] before:absolute before:inset-0 before:bg-radial-faded before:opacity-[0.4]",
+          "after:absolute after:-left-1/2 after:top-1/2 after:h-[142.8%] after:w-[200%] after:rounded-[50%] after:border-t after:border-[rgba(120,_119,_198,_0.4)] after:bg-background",
+        )}
+      >
+        <StarsIllustration />
+      </div>
     </>
   );
 }
