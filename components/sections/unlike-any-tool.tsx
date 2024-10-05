1286d4acdf78e908fa2c5488b3c8c6321fdf41cb
Gabriel Samoylov <gsamo122@gmail.com>
2024-10-05 21:40:28 +0300

    adding the tiles to the USP section

--- components/sections/unlike-any-tool.tsx
+++ components/sections/unlike-any-tool.tsx
@@ -11,6 +11,33 @@ export const UnlikeAnyTool = () => {
           Linear combines UI elegance with world-class performance.
         </p>
       </div>
+      <div className="flex flex-wrap gap-6">
+        <div className="basis-[calc(66.66%-12px)] rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:p-14">
+          <p className="text-3xl">Built for your keyboard</p>
+          <p className="text-md text-primary-text">
+            Fly through your tasks with rapid-fire keyboard shortcuts for
+            everything. Literally everything.
+          </p>
+        </div>
+        <div className="basis-[calc(33.33%-12px)] rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:p-14">
+          <p className="text-3xl">Breathtakingly fast</p>
+          <p className="text-md text-primary-text">
+            Built for speed with 50ms interactions and real-time sync.
+          </p>
+        </div>
+        <div className="basis-[calc(33.33%-12px)] rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:p-14">
+          <p className="text-3xl">Designed for modern software teams</p>
+          <p className="text-md text-primary-text">
+            Comes with built-in workflows that create focus and routine.
+          </p>
+        </div>
+        <div className="basis-[calc(66.66%-12px)] rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:p-14">
+          <p className="text-3xl">Meet your command line</p>
+          <p className="text-md text-primary-text">
+            Complete any action in seconds with the global command menu.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
