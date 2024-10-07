c79a2ef5dba302c2af48d79082676f0d8225fd6d
Gabriel Samoylov <gsamo122@gmail.com>
2024-10-07 19:13:08 +0300

    shortcuts auto animation. making the keyboard responsive.

--- components/keyboard-shortcuts.tsx
+++ components/keyboard-shortcuts.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { MouseEvent, useRef } from "react";
+import { MouseEvent, useEffect, useRef } from "react";
 import { Button, Highlight } from "@/components/button";
 import { KeyboardIllustration } from "@/components/illustrations/keyboard";
 
@@ -19,18 +19,33 @@ const shortcuts = [
 ];
 
 export const KeyboardShortcuts = () => {
+  const activeShortcutIndex = useRef(0);
+  const timeoutRef = useRef<NodeJS.Timeout>();
   const wrapperRef = useRef<HTMLDivElement>(null);
   const illustrationWrapperRef = useRef<HTMLDivElement>(null);
 
-  const onShortcutButtonClick = (
-    e: MouseEvent<HTMLButtonElement>,
-    keys: string,
-  ) => {
-    e.preventDefault();
+  const scheduleTimeout = () => {
+    timeoutRef.current = setTimeout(goToNextShortcut, 2500);
+  };
+
+  useEffect(() => {
+    scheduleTimeout();
+    return () => clearTimeout(timeoutRef.current);
+  }, []);
+
+  const goToShortcut = (index: number) => {
+    clearTimeout(timeoutRef.current);
+
     if (!wrapperRef.current) return;
 
+    const shortcut = wrapperRef.current.querySelector<HTMLButtonElement>(
+      `button:nth-child(${index + 1})`,
+    );
+
+    if (!shortcut) return;
+
     wrapperRef.current.scrollTo({
-      left: e.currentTarget.offsetLeft - wrapperRef.current.clientWidth / 2,
+      left: shortcut.offsetLeft - wrapperRef.current.clientWidth / 2,
       behavior: "smooth",
     });
 
@@ -40,30 +55,47 @@ export const KeyboardShortcuts = () => {
       .querySelectorAll(".active")
       .forEach((el) => el.classList.remove("active"));
 
+    const keys = shortcut.dataset.keys || "";
     const keyArray = keys.split("");
     const keyElements = keyArray.map((key) =>
       illustrationWrapperRef.current?.querySelector(`[data-key="${key}"]`),
     );
 
     keyElements.forEach((element) => element?.classList.add("active"));
+
+    activeShortcutIndex.current = index;
+    scheduleTimeout();
+  };
+
+  const goToNextShortcut = () =>
+    goToShortcut((activeShortcutIndex.current + 1) % shortcuts.length);
+
+  const onShortcutButtonClick = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    goToShortcut(Number(e.currentTarget.dataset.index));
   };
 
   return (
     <>
-      <div ref={illustrationWrapperRef} className="mask-keyboard h-full w-full">
+      <div
+        ref={illustrationWrapperRef}
+        className="mask-keyboard absolute -left-12 top-0 h-full max-h-[30rem] w-[200%] md:relative md:left-auto md:w-full"
+      >
         <KeyboardIllustration />;
       </div>
-      <div className="my-7 min-h-[4rem] w-full overflow-hidden">
+      <div className="my-7 hidden min-h-[4rem] w-full overflow-hidden md:block">
         <div
           ref={wrapperRef}
           className="mask-shortcutkeys flex min-h-[4rem] max-w-full snap-x snap-mandatory gap-2 overflow-x-auto whitespace-nowrap pb-8"
         >
-          {shortcuts.map((shortcut) => (
+          {shortcuts.map((shortcut, index) => (
             <Button
               className="shrink-0 snap-center first:ml-[50vw] last:mr-[50vw]"
               key={shortcut.text}
               varient="secondary"
-              onClick={(e) => onShortcutButtonClick(e, shortcut.keys)}
+              data-index={index}
+              data-keys={shortcut.keys}
+              onClick={onShortcutButtonClick}
             >
               <Highlight className="uppercase">{shortcut.keys}</Highlight>
               <span>{shortcut.text}</span>
