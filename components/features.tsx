8b34bad4deeeb2b34953e55eeb39f7b0b3c0d8d8
Gabriel Samoylov <gsamo122@gmail.com>
2024-10-08 21:20:08 +0300

    features margin fix

--- components/features.tsx
+++ components/features.tsx
@@ -10,13 +10,15 @@ type FeatureProps = {
   colorDark: string;
 };
 
+(" relative flex flex-col items-center overflow-x-clip before:pointer-events-none before:absolute before:h-[40rem] before:w-full before:bg-[conic-gradient(from_90deg_at_80%_50%,#000212,rgb(var(--feature-color-dark))),conic-gradient(from_270deg_at_20%_50%,rgb(var(--feature-color-dark)),#000212)] before:bg-no-repeat before:transition-[transform,opacity] before:duration-1000 before:ease-in before:[mask:radial-gradient(100%_50%_at_center_center,_black,_transparent)] before:[background-size:50%_100%,50%_100%] before:[background-position:1%_0%,99%_0%] after:pointer-events-none after:absolute after:inset-0");
+
 export const Features = ({ children, color, colorDark }: FeatureProps) => {
-  const { ref, inView } = useInView({ threshold: 0.3, triggerOnce: false });
+  const { ref, inView } = useInView({ threshold: 0.2, triggerOnce: false });
   return (
     <section
       ref={ref}
       className={classNames(
-        "relative flex flex-col items-center overflow-x-hidden py-[12.8rem]",
+        "relative flex flex-col items-center overflow-x-clip",
         "before:feature-top-before before:pointer-events-none before:absolute before:h-[40rem] before:w-full before:transition-[transform,opacity] before:duration-1000 before:ease-in",
         "after:bg-feature-gradient after:pointer-events-none after:absolute after:inset-0",
         inView
@@ -32,7 +34,7 @@ export const Features = ({ children, color, colorDark }: FeatureProps) => {
         } as React.CSSProperties
       }
     >
-      <div className="my-[25.2rem]">{children}</div>
+      <div className="mt-[25.2rem]">{children}</div>
     </section>
   );
 };
